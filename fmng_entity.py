ffb07709661b5f2ac95320fdf5002182f65bef4e
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-04-25 12:36:58 -0300

    Updated file "fmng_entity.py".

--- fmng_entity.py
+++ fmng_entity.py
@@ -56,7 +56,9 @@ def fmng_entity_export_sqlite(client, args, db_path):
             parent_id INTEGER,
             ct_url,
             is_album,
-            create_date,
+            inclusion_date,
+            entity_code,
+            path,
             new_id INTEGER
             );
     ''')
@@ -72,8 +74,9 @@ def fmng_entity_export_sqlite(client, args, db_path):
         if fmng_entity_reg.parent_id is not False:
             parent_id = fmng_entity_reg.parent_id.id
 
-        print(fmng_entity_count, fmng_entity_reg.id, parent_id, fmng_entity_reg.entity_code_base,
-              fmng_entity_reg.name.encode("utf-8"))
+        print(fmng_entity_count, fmng_entity_reg.id, parent_id, fmng_entity_reg.entity_code_base, fmng_entity_reg.entity_code,
+              fmng_entity_reg.inclusion_date, fmng_entity_reg.name.encode("utf-8")
+              )
 
         cursor.execute('''
                        INSERT INTO ''' + table_name + '''(
@@ -89,9 +92,11 @@ def fmng_entity_export_sqlite(client, args, db_path):
                            parent_id,
                            ct_url,
                            is_album,
-                           create_date
+                           inclusion_date,
+                           entity_code,
+                           path
                            )
-                       VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)''',
+                       VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''',
                        (fmng_entity_reg.id,
                         fmng_entity_reg.name,
                         fmng_entity_reg.alias,
@@ -104,9 +109,93 @@ def fmng_entity_export_sqlite(client, args, db_path):
                         parent_id,
                         fmng_entity_reg.ct_url,
                         fmng_entity_reg.is_album,
-                        str(fmng_entity_reg.create_date)
+                        str(fmng_entity_reg.inclusion_date),
+                        fmng_entity_reg.entity_code,
+                        fmng_entity_reg.path,
                         )
                        )
+
+    conn.commit()
+    conn.close()
+
+    print()
+    print('--> fmng_entity_count: ', fmng_entity_count)
+
+
+def fmng_entity_import_sqlite(client, args, db_path):
+
+    table_name = 'fmng_entity'
+
+    conn = sqlite3.connect(db_path)
+    conn.text_factory = str
+
+    cursor = conn.cursor()
+
+    cursor2 = conn.cursor()
+
+    data = cursor.execute('''
+        SELECT
+            id,
+            name,
+            alias,
+            entity_code_base,
+            image,
+            url,
+            description,
+            info,
+            entity_code_size,
+            parent_id,
+            ct_url,
+            is_album,
+            inclusion_date,
+            entity_code,
+            path,
+            new_id
+        FROM ''' + table_name + ''';
+    ''')
+
+    clv_file = client.model('clv_file')
+
+    print(data)
+    print([field[0] for field in cursor.description])
+    fmng_entity_count = 0
+    for row in cursor:
+        fmng_entity_count += 1
+
+        print(fmng_entity_count, row[0], row[1], row[2], row[3], '', row[5],
+              row[6], '', row[8], row[9], row[10], row[11], row[12], row[15])
+
+        values = {
+            'name': row[1],
+            # 'alias': row[2],
+            'alias': row[14],
+            # 'code': row[3],
+            'code': row[13],
+            'description': row[6],
+            'notes': row[7],
+            'date_inclusion': row[12],
+            'active': True,
+            'url': row[5],
+            'ct_url': row[10],
+            }
+        file_id = clv_file.create(values).id
+
+        try:
+            values = {
+                'image': row[4],
+                }
+            clv_file.write(file_id, values)
+        except Exception as e:
+            print('>>>>>', e)
+
+        cursor2.execute('''
+                       UPDATE ''' + table_name + '''
+                       SET new_id = ?
+                       WHERE id = ?;''',
+                        (file_id,
+                         row[0]
+                         )
+                        )
 
     conn.commit()
     conn.close()
